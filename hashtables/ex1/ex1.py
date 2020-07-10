1f27531a486eda6265e2a205f812124e7aa30be0
RobertMisch <robertmisch94@gmail.com>
2020-07-10 12:31:08 -0400

    finished 2

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,34 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    # so first up is making a dictionary with all the value combinations
+    combinations={}
+    for i in range(limit+1):
+        combinations[i]= limit-i
+    # print(combinations)
+    #going to also make our list a dictionary for O(1) lookup
+    weights_dict={}
+    for index, value in enumerate(weights):
+        if value in weights_dict:
+            weights_dict[value].append(index)
+        elif value<=limit:
+            weights_dict[value]=[index]
+    #now we loop through weights and see which match the table
+    for item in weights:
+        if item >= limit:
+            continue
+        if combinations[item] in weights_dict:
+            if(len(weights_dict[combinations[item]]) > 1):
+                # print(weights_dict[combinations[item]])
+                result = weights_dict[combinations[item]].sort(reverse=True)
+                return (weights_dict[combinations[item]])
+            else:
+                result= weights_dict[item] + weights_dict[combinations[item]]
+                result.sort(reverse=True)
+                return(result)
 
+        
+    # print(weights_dict)
     return None
+weights_2 = [4, 5]
+print(get_indices_of_item_weights(weights_2, 2, 9))
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,30 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
-
+    route=[]
+    ticket_index={}
+    #build ticket index
+    for ticket in tickets:
+        ticket_index[ticket.source] = ticket.destination
+    #get trip starter
+    route.append(ticket_index["NONE"])
+    for i in range(length-1):
+        current_ticket= route[-1]
+        route.append(ticket_index[current_ticket])
     return route
+
+    # return route
+
+
+# tickets = [
+#     Ticket{ source: "PIT", destination: "ORD" },
+#     Ticket{ source: "XNA", destination: "CID" },
+#     Ticket{ source: "SFO", destination: "BHM" },
+#     Ticket{ source: "FLG", destination: "XNA" },
+#     Ticket{ source: "NONE", destination: "LAX" },
+#     Ticket{ source: "LAX", destination: "SFO" },
+#     Ticket{ source: "CID", destination: "SLC" },
+#     Ticket{ source: "ORD", destination: "NONE" },
+#     Ticket{ source: "SLC", destination: "PIT" },
+#     Ticket{ source: "BHM", destination: "FLG" }
+# ]
